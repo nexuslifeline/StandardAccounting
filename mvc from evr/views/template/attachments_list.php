6ab249fcff690fb283a19f54fa650ba57781adb2
Paul Christian Rueda <nexuslifeline@yahoo.com>
2017-03-04 12:30:31 +0800

    Remove Unnecessary files

--- mvc from evr/views/template/attachments_list.php
+++
@@ -1,9 +0,0 @@
-<br />
-<div class="row">
-    <div class="col-lg-12">
-        <strong>Attachments</strong>
-        <hr />
-        <br />
-        <a href="#"><i class="fa fa-file"></i> Schedule 2016.pdf</a>
-    </div>
-</div>
